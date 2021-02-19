dc964d3ae53a8a846cb6bb4a8110ce97dafb2bfb
Alexandre Simoneau <alsimoneau@gmail.com>
2021-02-19 13:56:03 -0500

    Added dark combiner

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ from astropy import units as u
 from astropy.nddata import CCDData
 import exiftool
 
-def open_raw(fname):
+def open_raw(fname, normalize=False):
     raw = rawpy.imread(fname)
     rgb = raw.postprocess(
         gamma = (1,1),
@@ -20,13 +20,25 @@ def open_raw(fname):
     except KeyError:
         exposure = float(exif['EXIF:ExposureTime'])
 
+    if normalize:
+        rgb /= 2**16 - 1
+
     data = CCDData(rgb,unit=u.adu)
     data.header['exposure'] = exposure
     return data
 
 def open_dark(fnames):
-    #ccdproc.combine
-    pass
+    frames = [ open_raw(fname) for fname in fnames ]
+    if frames[0].data.ndim == 3:
+        dark = np.stack( [
+            ccdproc.combine([f[:,:,i] for f in frames], sigma_clip=True) \
+            for i in range(3)
+        ], axis=2 )
+        dark = CCDData(dark,unit=u.adu)
+        dark.header = frames[0].header
+    else:
+        dark = ccdproc.combine(frames, sigma_clip=True)
+    return dark
 
 def substract_dark(data,dark):
     #ccdproc.substract_dark
@@ -42,8 +54,10 @@ def cosmicray_removal(image,**kwargs):
     if image.data.ndim == 3:
         new_data = np.stack( [
             ccdproc.cosmicray_lacosmic(image.data[:,:,i],**kwargs)[0] \
-            for i in range(image.data.shape[2])
+            for i in range(3)
         ], axis=2 )
     else:
         new_data = ccdproc.cosmicray_lacosmic(image.data,**kwargs)[0]
-    return CCDData(new_data,unit=u.adu)
+    new_data = CCDData(new_data,unit=u.adu)
+    new_data.header = image.header
+    return new_data
