Fixed underflow when substracting dark

linearity.py
@@ -14,6 +14,7 @@ import numpy as np
 from glob import glob
 import exiftool
 from utils import open_clipped as Open
+from utils import sub
 
 @click.command(name="linearity")
 def CLI_linearity():
@@ -31,11 +32,11 @@ def linearity():
             exif = et.get_metadata(glob(f"LINEARITY/{ss}_*")[0])
         exp = exif['MakerNotes:SonyExposureTime2']
         dark = Open(glob(f"LINEARITY/DARKS/{ss}_*"))
-        frame -= dark
+        frame = sub(frame,dark)
 
         print(exp,*frame.mean((0,1)))
         data.append((exp,*frame.mean((0,1))))
 
     data = np.asarray(data,dtype=float)
     #data = data[np.argsort(data[:,0])]
-    np.savetxt("data",data)
+    np.savetxt("linearity.dat",data)

utils.py
@@ -12,12 +12,12 @@ def open_raw(fname, normalize=False):
         no_auto_bright = True,
         user_flip = 0
     )
-    with exiftool.ExifTool() as et:
-        exif = et.get_metadata(fname)
-    try:
-        exposure = float(exif['MakerNotes:SonyExposureTime'])
-    except KeyError:
-        exposure = float(exif['EXIF:ExposureTime'])
+    # with exiftool.ExifTool() as et:
+    #     exif = et.get_metadata(fname)
+    # try:
+    #     exposure = float(exif['MakerNotes:SonyExposureTime'])
+    # except KeyError:
+    #     exposure = float(exif['EXIF:ExposureTime'])
 
     if normalize:
         rgb /= 2**16 - 1
@@ -49,7 +49,7 @@ def compute_stats(fnames):
 
 def open_clipped(fnames,mean=None,stdev=None,sigclip=5):
     if type(fnames) == str or len(fnames) == 0:
-        raise(TypeError("open_clipped expects a list of strings.")) 
+        raise(TypeError("open_clipped expects a list of strings."))
     if mean is None or stdev is None:
         print("Computing statistics...")
         mean,stdev = compute_stats(fnames)
@@ -72,3 +72,6 @@ def cosmicray_removal(image,**kwargs):
     else:
         new_data = detect_cosmics(image,**kwargs)[1]
     return new_data
+
+def sub(frame,dark):
+    return frame.astype(np.int32)-dark