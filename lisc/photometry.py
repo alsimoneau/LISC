80777d32672bbcd4beec61d09eb8bd1c70c60b55
Alexandre Simoneau <alsimoneau@gmail.com>
2021-11-26 13:56:34 +0000

    Updated molecular aod model

--- lisc/photometry.py
+++ lisc/photometry.py
@@ -41,65 +41,20 @@ def CLI_photometry(radius, drift_window):
 # TODO: Use astrometry for star identification
 
 
-def lowtran(wls, pressure, altitude, altitude_pressure):
-    Tm_exp = np.exp(
-        -(pressure / 101.3) / ((wls / 1000) ** 4 * 115.6406)
-        - (wls / 1000) ** 2 * 1.335
+def lowtran(wls):
+    """Railegh molecular optical depth from eq.30 of Kneizys (1980).
+    Expects wls in µm."""
+    return 1 / ((wls ** 4 * 115.6406) - wls ** 2 * 1.335)
+
+
+def aeronet(wls):
+    """Railegh molecular optical depth from eq.30 of Bodhaide (1999).
+    Expects wls in µm."""
+    return (
+        0.0021520
+        * (1.0455996 - 341.29061 * wls ** (-2) - 0.90230850 * wls ** 2)
+        / (1 + 0.0027059889 * wls ** (-2) - 85.968563 * wls ** 2)
     )
-    Tm_inf = Tm_exp ** np.exp(altitude_pressure / 8000)
-    return Tm_inf ** np.exp(-altitude / 8000)
-
-
-def bodhaine(wls, pressure, altitude, altitude_pressure, CO2, latitude):
-    pressure *= (1 - 0.0065 * (altitude - altitude_pressure) / 288.15) ** (
-        (-9.80665 * 0.0289644) / (8.3144598 * -0.0065)
-    )
-
-    FO2 = 1.096 + 1.385e-3 * wls ** -2 + 1.448e-4 * wls ** -4
-    FN2 = 1.034 + 3.17e-4 * wls ** -2
-    Fair = (78.084 * FN2 + 20.946 * FO2 + 0.934 * 1.00 + CO2 * 1.15) / (
-        78.084 + 20.946 + 0.934 + CO2
-    )
-    n300m1 = 1e-8 * (
-        8060.51
-        + 2480990 / (132.274 - (wls / 1e3) ** -2)
-        + 17455.7 / (39.32957 - (wls / 1e3) ** -2)
-    )  # lambda in um
-    nCO2 = 1 + (n300m1 * (1 + 0.54 * (CO2 / 1e6 - 0.0003)))
-    Ns = N_A / 22.4141 * 273.15 / 288.15 * 1e-3  # mol/cm^3
-    sig = (
-        (24 * np.pi ** 3 * (nCO2 ** 2 - 1) ** 2)
-        / ((wls / 1e7) ** 4 * Ns ** 2 * (nCO2 ** 2 + 2) ** 2)
-        * ((6 + 3 * Fair) / (6 - 7 * Fair))
-    )  # lambda in cm
-
-    gasses = np.array(
-        [
-            (78.084, 28.013),  # N2ROD
-            (20.946, 31.999),  # O2
-            (0.934, 39.948),  # Ar
-            (1.80e-3, 20.18),  # Ne
-            (5.20e-4, 4.003),  # He
-            (1.10e-4, 83.8),  # Kr
-            (5.80e-5, 2.016),  # H2
-            (9.00e-6, 131.29),  # Xe
-            (100 * CO2 / 1e6, 44.01),  # CO2
-        ]
-    )
-    ma = np.dot(*gasses.T) / np.sum(gasses[:, 0])
-
-    c2l = np.cos(2 * np.deg2rad(latitude))
-    z = 0.73737 * altitude + 5517.56
-    g = (
-        (980.6160 * (1 - 2.6373e-3 * c2l + 5.9e-6 * c2l ** 2))
-        - (3.085462e-4 + 2.27e-7 * c2l) * z
-        + (7.254e-11 + 1.0e-13 * c2l) * z ** 2
-        - (1.517e-17 + 6e-20 * c2l) * z ** 3
-    )  # cm/s^2
-
-    rod = sig * ((pressure * 1e4) * N_A) / (ma * g)  # kPa -> dyn/cm^2
-
-    return np.exp(-rod)
 
 
 def photometry(r=50, drift_window=200):
@@ -162,14 +117,15 @@ def photometry(r=50, drift_window=200):
     wls /= 10  # A -> nm
     star *= 1e-2  # ergs / s / cm^2 / A -> W / m^2 / nm
 
-    Tm_exp = lowtran(wls, p["pressure"], p["altitude"], p["altitude_pressure"])
-    Tm = Tm_exp ** np.exp(1 / np.cos(np.deg2rad(p["theta"])))
+    cost = np.cos(np.deg2rad(p["theta"]))
+
+    Tm_exp = np.exp(-p["pressure"] / 101.325 * aeronet(wls / 1000))  # in µm
+    Tm_inf = Tm_exp ** (1 / np.exp(-p["altitude_pressure"] / 8000))
+    Tm = Tm_inf ** (np.exp(-p["altitude"] / 8000) / cost)
 
     Ta_exp = np.exp(-p["aod"] * (wls / 500) ** (-p["alpha"]))
     Ta_inf = Ta_exp ** (1 / np.exp(-p["altitude_aod"] / 2000))
-    Ta = Ta_inf ** (
-        np.exp(-p["altitude"] / 2000) / np.cos(np.deg2rad(p["theta"]))
-    )
+    Ta = Ta_inf ** (np.exp(-p["altitude"] / 2000) / cost)
 
     with open("photometry.dat", "w"):
         pass
