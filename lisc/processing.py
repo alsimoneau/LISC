3a2f7420806e70188a37b05521b1c338dfd8097f
Alexandre Simoneau <alsimoneau@gmail.com>
2022-11-10 14:28:30 -0500

    Input format fix

--- lisc/processing.py
+++ lisc/processing.py
@@ -14,6 +14,7 @@ import lisc.utils
     "-p",
     "--percentile",
     default=[100, 99, 95, 90],
+    type=float,
     multiple=True,
     help="The percentile value to extract. Can be specified multiple times.",
 )
